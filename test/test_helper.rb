69e9e06268b9f9b69d85470759cd55f271aec648
Andrey Moshkov <fakesupp@gmail.com>
2023-07-29 22:03:23 +0300

    :technologist: Add helper metrhod for read fixture

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,6 +16,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def load_fixture(filename)
+    File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
+  end
 end
 
 class ActionDispatch::IntegrationTest
