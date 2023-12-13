5a2d04dccb52a8349eaf199cb2d86def2e3bd6d9
Andrey Moshkov <fakesupp@gmail.com>
2023-12-13 18:06:45 +0300

    :heavy_plus_sign: Add base pundit settings

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -2,4 +2,14 @@
 
 class Web::ApplicationController < ApplicationController
   include AuthConcern
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = I18n.t('not_authorized')
+    redirect_back(fallback_location: root_path)
+  end
 end
