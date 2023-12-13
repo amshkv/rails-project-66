25b428689aab9c34e562c3bdedb9d8fc9b9f219d
Andrey Moshkov <fakesupp@gmail.com>
2023-12-13 18:04:34 +0300

    :wastebasket: Delete unused test method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,9 +22,6 @@ class ActiveSupport::TestCase
   end
 
   # Add more helper methods to be used by all tests here...
-  def load_fixture(filename)
-    File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
-  end
 end
 
 class ActionDispatch::IntegrationTest
