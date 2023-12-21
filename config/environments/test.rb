e4c9bfd8db0111296cb4fa5add075d9c1fb03495
Andrey Moshkov <fakesupp@gmail.com>
2023-12-22 00:39:31 +0300

    :white_check_mark: Fix tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,4 +65,6 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  routes.default_url_options = { host: 'lalajopa.app' }
 end
