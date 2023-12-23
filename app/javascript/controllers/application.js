78cb980e2b0e7037ba0601c343cfe42212c19781
Andrey Moshkov <fakesupp@gmail.com>
2023-12-23 21:48:09 +0300

    :rotating_light: Fix eslint error

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,7 @@ const application = Application.start();
 
 // Configure Stimulus development experience
 application.debug = false;
+// eslint-disable-next-line no-undef
 window.Stimulus = application;
 
 export default application;
