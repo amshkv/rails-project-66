64cba28a5406c32139cc1d7c57cf840a776aeb2b
Andrey Moshkov <fakesupp@gmail.com>
2023-12-23 06:47:28 +0300

    :white_check_mark: Add test to api check create

---
+++ test/controllers/api/checks_controller_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
+  test 'api check create' do
+    webhook_payload = File.read('test/fixtures/files/webhook_payload.json')
+    repository = repositories(:without_checks)
+    # NOTE: изобретаем фабрику?
+    payload = JSON.parse(webhook_payload)
+
+    payload['repository']['id'] = repository.github_id.to_i
+
+    post api_checks_path, as: :json, headers: { 'x-github-event': 'push' }, params: payload
+
+    assert_response :success
+
+    check = Repository::Check.find_by repository: repository.id
+    assert_response :success
+    assert { check }
+    assert { check.finished? }
+    assert { check.success_check == false }
+    assert { check.lint_messages_count == 4 }
+  end
+end
