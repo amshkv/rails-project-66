37c0d5012e840cff3c9d8b819aada7cffa2de919
Andrey Moshkov <fakesupp@gmail.com>
2023-12-24 10:49:27 +0300

    :recycle: Move base email to env

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@mshkv.ru'
+  default from: ENV.fetch('BASE_MAIL', 'no-reply@localhost')
   layout 'mailer'
 end
