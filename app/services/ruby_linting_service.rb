bc4b9d36b2f3ce48fa83f352ee6935d938273dda
Andrey Moshkov <fakesupp@gmail.com>
2023-12-25 01:02:56 +0300

    :bug: Fix bug

--- app/services/ruby_linting_service.rb
+++ app/services/ruby_linting_service.rb
@@ -9,21 +9,21 @@ class RubyLintingService
     def lint_messages_count(json)
       json['summary']['offense_count']
     end
-  end
 
-  def serialize_errors(json)
-    files = json['files']
-    files_with_errors = files.filter { |file| file['offenses'].any? }
-    files_with_errors.map do |file|
-      errors = file['offenses'].map do |error|
-        {
-          message: error['message'],
-          rule: error['cop_name'],
-          position: "#{error['location']['line']}:#{error['location']['column']}"
-        }
+    def serialize_errors(json)
+      files = json['files']
+      files_with_errors = files.filter { |file| file['offenses'].any? }
+      files_with_errors.map do |file|
+        errors = file['offenses'].map do |error|
+          {
+            message: error['message'],
+            rule: error['cop_name'],
+            position: "#{error['location']['line']}:#{error['location']['column']}"
+          }
+        end
+        file_path = file['path']
+        { file_path:, errors: }
       end
-      file_path = file['path']
-      { file_path:, errors: }
     end
   end
 end
